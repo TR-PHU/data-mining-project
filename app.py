5aee53e39a0829d58bb6c58c2550b3fcc414c5be
Phu Tran <tranphutptv@gmail.com>
2023-06-11 17:30:32 +0700

    use pca to reduce dimensions

--- app.py
+++ app.py
@@ -2,73 +2,96 @@ from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 from sklearn.mixture import GaussianMixture
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, normalize, OrdinalEncoder
+from sklearn.decomposition import PCA
 
 app = Flask(__name__)
 
-def load_data(filename):
-    data = pd.read_csv(filename)
-    selected_columns = ['age', 'job', 'marital', 'education', 'balance', 'housing', 'loan']
-    data = data[selected_columns]
-    return data
-
-def preprocess_data(data):    
-    categorical_cols = ['job', 'marital', 'education', 'housing', 'loan']
+class GMM_model:
+    __model = None
+    __encoder = OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1)
+    __scaler = StandardScaler() 
+    __pca =  PCA(2) 
     
-    label_encoders = {}
-    for col in categorical_cols:
-        label_encoders[col] = LabelEncoder()
-        data[col] = label_encoders[col].fit_transform(data[col])
+    def __init__(self):
+        df = pd.read_csv("./bank.csv")
+        df = df.drop('contact', axis=1)
+        df = df.dropna()
+
+        X_principal = self.__init_preprocessing(df)
+        
+        # Fitting model 
+        self.__model = GaussianMixture(n_components=4).fit(X_principal)
+        
+    def predict(self, df):
+        # Encoding df
+        encoded_df = self.__encoder.transform(df)
+        encoded_df = pd.DataFrame(encoded_df)
+        encoded_df.columns = df.columns.tolist()      
+        
+        print("Encoded df: ", encoded_df)
+        # Scaling
+        scaled_df = self.__scaler.transform(encoded_df) 
+        
+        # Normalizing 
+        normalized_df = normalize(scaled_df) 
+        normalized_df = pd.DataFrame(normalized_df) 
+        
+        # Reducing the dimensions of the data 
+        X_principal = self.__pca.transform(normalized_df) 
+        X_principal = pd.DataFrame(X_principal) 
+        
+        return self.__model.predict(X_principal) 
     
-    scaler = StandardScaler()
-    data[['age', 'balance']] = scaler.fit_transform(data[['age', 'balance']])
-    
-    return data.values
+    def __init_preprocessing(self, df):
+        # Encoding df
+        self.__encoder.fit(df)
+        encoded_df = self.__encoder.transform(df)
+        encoded_df = pd.DataFrame(encoded_df)
+        encoded_df.columns = df.columns.tolist()
 
-def train_gmm(X, num_components):
-    gmm = GaussianMixture(n_components=num_components, random_state=0)
-    gmm.fit(X)
-    return gmm
-
-def predict_segmentation(gmm, new_customer):
-    return gmm.predict(new_customer.reshape(1, -1))
-
-def add_and_preprocess_data(df, new_data):
-    new_df = pd.DataFrame([new_data])
-
-    df = df.append(new_df, ignore_index=True)
-
-    preprocessed_df = preprocess_data(df)
-
-    return preprocessed_df
-
-filename = 'bank.csv'
-data = load_data(filename)
-
-preprocessed_data = preprocess_data(data)
-num_components = 3
-gmm = train_gmm(preprocessed_data, num_components)
+        # Scaling
+        self.__scaler.fit(encoded_df)
+        scaled_df = self.__scaler.transform(encoded_df) 
+        
+        # Normalizing 
+        normalized_df = normalize(scaled_df) 
+        normalized_df = pd.DataFrame(normalized_df) 
+        
+        # Reducing the dimensions of the data 
+        self.__pca.fit(normalized_df)
+        X_principal = self.__pca.transform(normalized_df) 
+        X_principal = pd.DataFrame(X_principal) 
+        
+        return X_principal
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
 
-        # Chuẩn bị dữ liệu để tiền xử lý
-        customer_data = {
-            'age': int(request.form['age']),
-            'job': request.form['job'],
-            'marital': request.form['marital'],
-            'education': request.form['education'],
-            'balance': int(request.form['balance']),
-            'housing': request.form['housing'],
-            'loan': request.form['loan']
-        }
-        data = load_data(filename)
-        data = add_and_preprocess_data(data, customer_data)
+        age = int(request.form['age'])
+        job = request.form['job']
+        marital = request.form['marital']
+        education = request.form['education']
+        default = request.form['default']
+        balance = int(request.form['balance'])
+        housing = request.form['housing']
+        loan = request.form['loan']
+        day = int(request.form['day'])
+        month = request.form['month']
+        duration = int(request.form['duration'])
+        campaign = int(request.form['campaign'])
+        pdays = int(request.form['pdays'])
+        previous = int(request.form['previous'])
+        poutcome = request.form['poutcome']
+        deposit = request.form['deposit']
 
-        # Dự đoán phân đoạn cho khách hàng mới
-        new_customer = data[-1]
-        segment = predict_segmentation(gmm, new_customer)
+        new_customer = pd.DataFrame([[age, job, marital, education, default, balance, housing, loan,
+                        day, month, duration, campaign, pdays, previous, poutcome, deposit]])
+        new_customer.columns = pd.read_csv('./bank.csv').drop(['contact'], axis=1).columns.tolist()
+
+        model = GMM_model()
+        segment = model.predict(new_customer)
         
         return render_template('index.html', segment=segment[0]+1)
     
